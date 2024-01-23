27bec516b233ada376e3b1e39c6f9736ed4ce202
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2024-01-23 13:56:33 +0800

    kazuma.js

--- modules/commands/kazuma.js
+++ modules/commands/kazuma.js
@@ -57,7 +57,7 @@ module.exports.run = async function ({ api, event, args }) {
     api.setMessageReaction("🚀", event.messageID, (err) => {}, true);
     api.sendMessage(`𝙆𝙖𝙯𝙪𝙢𝙖𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜🔎: ${response}`, threadID, messageID);
 
-    const r = await kazumaAI.get(`https://scp-09.onrender.com/api/gpt?query=${response}`);
+    const r = await kazumaAI.get(`https://kazumaoff-peachwings.replit.app/api/gpt?query=${response}`);
     const kazuma = r.data.result;
 
     const c = "credits: www.facebook.com/devs150";
